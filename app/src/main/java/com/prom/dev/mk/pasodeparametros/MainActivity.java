6b513383cbfc71562a4ea3145238d82bc5a994fd
Marck <marckcarrion@gmail.com>
2018-10-31 09:58:25 +0100

    Creada la vista para las tabs
    Signed-off-by: Marck <marckcarrion@gmail.com>

--- app/src/main/java/com/prom/dev/mk/pasodeparametros/MainActivity.java
+++ app/src/main/java/com/prom/dev/mk/pasodeparametros/MainActivity.java
@@ -1,7 +1,10 @@
 package com.prom.dev.mk.pasodeparametros;
 /* Actividad 4*/
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Resources res = getResources();
+        TabHost tabs=(TabHost)findViewById( android.R.id.tabhost );
+        
+        tabs.setup();
+        TabHost.TabSpec spec=tabs.newTabSpec("mitab1");
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("TAB1");
+        tabs.addTab(spec);
+        spec=tabs.newTabSpec("mitab2");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("TAB2");
+        tabs.addTab(spec);
+        tabs.setCurrentTab(0);
+        tabs.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String s) {
+                Log.i("AndroidTabsDemo","Pulsada pestaña: "+s);
+            }
+        });
+
     }
 }
